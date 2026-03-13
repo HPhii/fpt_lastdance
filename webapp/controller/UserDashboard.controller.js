e98bfc1a3209688718c860517e9d99a118ec61f7
HuuAn03 <huuan2303@gmail.com>
2026-03-13 14:13:29 +0700

    SLA hit rate

--- webapp/controller/UserDashboard.controller.js
+++ webapp/controller/UserDashboard.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
   "./BaseController",
-  "sap/ui/model/json/JSONModel"
-], function (BaseController, JSONModel)
+  "sap/ui/model/json/JSONModel",
+  "sap/m/Popover",
+  "sap/m/Text"
+], function (BaseController, JSONModel, Popover, Text)
 {
   "use strict";
 
@@ -14,10 +16,34 @@ sap.ui.define([
       this._loadPerformanceData();
     },
 
+    onAfterRendering: function ()
+    {
+      this._detachSlaHoverEvents();
+      this._attachSlaHoverEvents();
+    },
+
+    onExit: function ()
+    {
+      this._detachSlaHoverEvents();
+      if (this._oSlaPopover)
+      {
+        this._oSlaPopover.destroy();
+        this._oSlaPopover = null;
+        this._oSlaPopoverText = null;
+      }
+    },
+
     _loadKPIData: function ()
     {
       var oView = this.getView();
       var oModel = this.getOwnerComponent().getModel("userKPIAnalytics");
+
+      oView.setModel(new JSONModel({
+        MyOpenTasksCount: "0",
+        DueTodayCount: "0",
+        MyOverdueCount: "0",
+        busy: true
+      }), "kpiModel");
 
       oModel.read("/ZC_GSP26SAP02_MYKPIACT", {
         success: function (oData)
@@ -29,7 +55,8 @@ sap.ui.define([
           var oKpiModel = new JSONModel({
             MyOpenTasksCount: oResult.MyOpenTasksCount,
             DueTodayCount: oResult.DueTodayCount,
-            MyOverdueCount: oResult.MyOverdueCount
+            MyOverdueCount: oResult.MyOverdueCount,
+            busy: false
           });
 
           oView.setModel(oKpiModel, "kpiModel");
@@ -39,7 +66,8 @@ sap.ui.define([
           var oKpiModel = new JSONModel({
             MyOpenTasksCount: "–",
             DueTodayCount: "–",
-            MyOverdueCount: "–"
+            MyOverdueCount: "–",
+            busy: false
           });
           oView.setModel(oKpiModel, "kpiModel");
         }
@@ -49,6 +77,66 @@ sap.ui.define([
     onNavBack: function ()
     {
       this.getOwnerComponent().getRouter().navTo("RouteDashboard");
+    },
+
+    _attachSlaHoverEvents: function ()
+    {
+      var oChartArea = this.byId("slaChartHoverArea");
+      if (!oChartArea)
+      {
+        return;
+      }
+
+      oChartArea.attachBrowserEvent("mouseenter", this._handleSlaMouseEnter, this);
+      oChartArea.attachBrowserEvent("mouseleave", this._handleSlaMouseLeave, this);
+    },
+
+    _detachSlaHoverEvents: function ()
+    {
+      var oChartArea = this.byId("slaChartHoverArea");
+      if (!oChartArea)
+      {
+        return;
+      }
+
+      oChartArea.detachBrowserEvent("mouseenter", this._handleSlaMouseEnter, this);
+      oChartArea.detachBrowserEvent("mouseleave", this._handleSlaMouseLeave, this);
+    },
+
+    _handleSlaMouseEnter: function ()
+    {
+      var oChartArea = this.byId("slaChartHoverArea");
+      var oAggregateModel = this.getView().getModel("kpiAggregateModel");
+      var iSlaHitRate = oAggregateModel ? oAggregateModel.getProperty("/slaHitRate") : 0;
+
+      if (!oChartArea)
+      {
+        return;
+      }
+
+      if (!this._oSlaPopover)
+      {
+        this._oSlaPopoverText = new Text();
+        this._oSlaPopover = new Popover({
+          showHeader: false,
+          placement: "Top",
+          contentWidth: "12rem",
+          content: [this._oSlaPopoverText]
+        });
+        this._oSlaPopover.addStyleClass("sapUiContentPadding");
+        this.getView().addDependent(this._oSlaPopover);
+      }
+
+      this._oSlaPopoverText.setText("SLA Hit Rate: " + iSlaHitRate + "%");
+      this._oSlaPopover.openBy(oChartArea);
+    },
+
+    _handleSlaMouseLeave: function ()
+    {
+      if (this._oSlaPopover)
+      {
+        this._oSlaPopover.close();
+      }
     },
 
     _loadPerformanceData: function ()
@@ -92,10 +180,12 @@ sap.ui.define([
 
           var fAvgProcessingTime = iCompleted > 0 ? (iTotalDays / iCompleted) : 0;
           var fSlaHitRate = iCompleted > 0 ? (iOnTime / iCompleted) * 100 : 0;
+          var fSlaRounded = Math.max(0, Math.min(100, Math.round(fSlaHitRate)));
 
           // Determine SLA color (for RadialMicroChart) and state (for ObjectStatus)
           var sSlaColor;
           var sSlaState;
+
           if (fSlaHitRate >= 95) {
             sSlaColor = "Good";
             sSlaState = "Success";
@@ -109,7 +199,7 @@ sap.ui.define([
 
           oAggregateModel.setData({
             avgProcessingTime: fAvgProcessingTime.toFixed(2),
-            slaHitRate: Math.round(fSlaHitRate),
+            slaHitRate: fSlaRounded,
             slaColor: sSlaColor,
             slaState: sSlaState,
             completedCount: iCompleted,
